54ea8b4a84a2442fe4c5c322b6ee37e3cb85cc2f
jalpp <jalp.panchal44@gmail.com>
2025-04-30 21:23:03 -0400

    fix(memory): add new memory updates

--- src/mastra/config/config.ts
+++ src/mastra/config/config.ts
@@ -7,8 +7,9 @@ import { google } from "@ai-sdk/google";
 import { anthropic } from '@ai-sdk/anthropic';
 import { perplexity } from '@ai-sdk/perplexity';
 import { LanguageModel } from "@mastra/core";
+import { LibSQLVector } from '@mastra/libsql';
 
-enum ModelType {
+enum ModelType { // add other one if needed
     GPT = "gpt",
     GEMINI = "gemini",
     CLAUDE = "claude",
@@ -17,7 +18,7 @@ enum ModelType {
 
 const CURRENT_MODEL: ModelType = ModelType.GPT; // change model
 
-function getCurrentModel(currentModel: ModelType): LanguageModel {
+export function getCurrentModel(currentModel: ModelType): LanguageModel {
     switch (currentModel) {
         case ModelType.GPT:
             return openai("gpt-4o");
@@ -28,12 +29,22 @@ function getCurrentModel(currentModel: ModelType): LanguageModel {
         case ModelType.PERPLEXITY:
             return perplexity('sonar-pro');
     }
-
-    return openai("gpt-4.0");
 }
 
 export const MODEL = getCurrentModel(CURRENT_MODEL);
-export const MEMORY = new Memory();
+export const MEMORY = new Memory({
+    options: {
+        lastMessages: 10,
+        semanticRecall: false,
+        threads: {
+          generateTitle: false
+        }
+      },
+    vector: new LibSQLVector({
+      connectionUrl: 'file:../memory.db'
+    })
+});
+
 export const EVALS = {answerRelevancy: new AnswerRelevancyMetric(MODEL), bias: new BiasMetric(MODEL), toxic: new ToxicityMetric(MODEL), tone: new ToneConsistencyMetric()}
 
 
